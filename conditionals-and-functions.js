93e57508e6fc2106e10d1c371e3fee9b651c6a69
Daniel Hernandez <dhernandez43@cnm.edu>
2019-09-23 15:50:46 -0600

    test

--- conditionals-and-functions.js
+++ conditionals-and-functions.js
@@ -1,9 +1,10 @@
 /*
-*Conditionals, Functions, Scopes, and Loops
+*Conditionals, Functions, Scope, and Loops
  */
 
+//Conditionals
 //Equals
-let equals = 1 ===1;
+let equals = 1 === 1;
 console.log(equals);
 
 //Greater than
@@ -20,3 +21,31 @@ let lessThanEq = 4 <= 9;
 
 //Not Equals
 let notEquals = 5 !== 2;
+
+let storeA = 4.40;
+let storeB = 4.40;
+
+//Functions
+function compareStorePrices(storeA, storeB) {
+	let storeAIsLower = storeA < storeB;
+	if(storeAIsLower) {
+		console.log("Store A has a lower price.")
+	} else if(storeB < storeA) {
+		console.log("Store B has a lower price.")
+	} else {
+		console.log("Their prices are equal.")
+	}
+}
+
+compareStorePrices(10,11);
+compareStorePrices(10,9);
+
+function squareNum (number) {
+	let squared = number * number;
+	return squared;
+}
+
+let squaredNumber = squareNum(12);
+console.log(squaredNumber);
+
+//Scope
